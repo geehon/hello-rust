5c111ed8dc013cae668dbd8d6c8048e0385a9a6c
geehon <liaoliaoliao70@gmail.com>
2022-11-23 15:47:19 +0800

    init grrs package

---
+++ grrs/target/debug/deps/grrs-51d909ec4ecf3f88.d
@@ -0,0 +1,5 @@
+/home/uta/github/hello-rust/grrs/target/debug/deps/grrs-51d909ec4ecf3f88: src/main.rs
+
+/home/uta/github/hello-rust/grrs/target/debug/deps/grrs-51d909ec4ecf3f88.d: src/main.rs
+
+src/main.rs:
---
+++ grrs/target/debug/grrs.d
@@ -0,0 +1,1 @@
+/home/uta/github/hello-rust/grrs/target/debug/grrs: /home/uta/github/hello-rust/grrs/src/main.rs
